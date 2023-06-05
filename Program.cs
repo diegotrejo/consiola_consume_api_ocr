116eea2c90cb0f667af93932ebef1a02f213842b
Diego Trejo <diego_trejo@hotmail.com>
2023-06-05 11:27:19 -0500

    Update Program.cs
    consume ocr desde arcivo local

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,27 +12,51 @@ namespace consiola_consume_api_ocr
     {
         static void Main(string[] args)
         {
+            //var ocr = ConsumeOCR_Url("https://i.pinimg.com/originals/39/8a/66/398a663074313bb6429c0bd3c8c646aa.jpg");
+            var ocr = ConsumeOCR_archivlo_local("d:\\foto.jpg");
+            Console.WriteLine(GetOcrText(ocr));
+            Console.ReadKey();
+        }
+
+        private static OCRresponse ConsumeOCR_archivlo_local(string nombre_archivo)
+        {
             var url = "https://eastus.api.cognitive.microsoft.com/vision/v3.2/ocr";
             var key = "d1c3a31c3e5243e8af964c1c95a01cb4";
             var par = "?language=es&detectOrientation=true&model-version=latest";
 
-            var foto = "https://i.pinimg.com/originals/39/8a/66/398a663074313bb6429c0bd3c8c646aa.jpg";
-            var json = Newtonsoft.Json.JsonConvert.SerializeObject( new 
-                { 
-                    Url = foto
-                }
-            );
+            var api = new WebClient();
+
+            api.Headers.Add("Content-Type", "application/octet-stream");
+            api.Headers.Add("Ocp-Apim-Subscription-Key", key);
             
+            var bytes = api.UploadFile(url + par, "POST", nombre_archivo);
+            var json = Encoding.UTF8.GetString(bytes);
+            var ocr = Newtonsoft.Json.JsonConvert.DeserializeObject<OCRresponse>(json);
+            return ocr;
+
+        }
+
+        private static OCRresponse ConsumeOCR_Url(string url_foto)
+        {
+            var url = "https://eastus.api.cognitive.microsoft.com/vision/v3.2/ocr";
+            var key = "d1c3a31c3e5243e8af964c1c95a01cb4";
+            var par = "?language=es&detectOrientation=true&model-version=latest";
+
+            var foto = url_foto; 
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(new
+            {
+                Url = foto
+            }
+            );
+
             var api = new WebClient();
 
             api.Headers.Add("Content-Type", "application/json");
             api.Headers.Add("Ocp-Apim-Subscription-Key", key);
-            json =  api.UploadString(url + par, "POST", json);
+            json = api.UploadString(url + par, "POST", json);
 
             var ocr = Newtonsoft.Json.JsonConvert.DeserializeObject<OCRresponse>(json);
-
-            Console.WriteLine(GetOcrText(ocr));
-            Console.ReadKey();
+            return ocr;
         }
 
         private static string GetOcrText(OCRresponse ocr)
@@ -47,7 +72,7 @@ namespace consiola_consume_api_ocr
                         //txt += Encoding.Unicode.GetString(encodedBytes) + " ";
                         txt += word.text + " ";
                     }
-                    txt += "\r\n";
+                    txt += " ";
                 }
                 txt += "\r\n";
             }
